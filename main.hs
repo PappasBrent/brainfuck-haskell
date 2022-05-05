83b3a88618bd91176caa0252f404eaaa24e6ff5a
Brent Pappas <pappasbrent@gmail.com>
2022-05-04 22:31:23 -0400

    Refactor evaluation functions to print to a string, not to stdout directly.
    Add functions for running tests.
    
    Add command line argument for running tests.

--- main.hs
+++ main.hs
@@ -59,51 +59,72 @@ mvLeft :: Tape -> Tape
 mvLeft (Tape (l : ls) x rs) = Tape ls l (x : rs)
 mvLeft _ = error "Error: Can't move left"
 
-loopBF :: Tape -> [BFCommand] -> IO Tape
-loopBF t@(Tape ls 0 rs) _ = return t
-loopBF t coms = do
-  t' <- evalBF t coms
-  loopBF t' coms
+loopBF :: String -> Tape -> [BFCommand] -> IO (String, Tape)
+loopBF s t@(Tape ls 0 rs) _ = return (s, t)
+loopBF s t bfcs = do
+  (s', t') <- evalBF s t bfcs
+  loopBF s' t' bfcs
 
-printCell :: Tape -> IO Tape
-printCell t@(Tape ls x rs) = do
-  putChar $ toEnum $ fromEnum x
-  return t
+printCell :: String -> Tape -> (String, Tape)
+printCell s t@(Tape ls x rs) = (s ++ [toEnum $ fromEnum x], t)
 
 readCell :: Tape -> IO Tape
 readCell t@(Tape ls x rs) = do
   c <- getChar
   return (Tape ls (toEnum $ ord c) rs)
 
-evalBF :: Tape -> [BFCommand] -> IO Tape
-evalBF t coms = do
-  case coms of
-    [] -> return t
+evalBF :: String -> Tape -> [BFCommand] -> IO (String, Tape)
+evalBF s t bfcs = do
+  case bfcs of
+    [] -> return (s, t)
     bc : bcs -> case bc of
-      IncCell -> evalBF (incCell t) bcs
-      DecCell -> evalBF (decCell t) bcs
-      MvRight -> evalBF (mvRight t) bcs
-      MvLeft -> evalBF (mvLeft t) bcs
+      IncCell -> evalBF s (incCell t) bcs
+      DecCell -> evalBF s (decCell t) bcs
+      MvRight -> evalBF s (mvRight t) bcs
+      MvLeft -> evalBF s (mvLeft t) bcs
       Loop bcs' -> do
-        t' <- loopBF t bcs'
-        evalBF t' bcs
+        (s', t') <- loopBF s t bcs'
+        evalBF s' t' bcs
       PrintCell -> do
-        t' <- printCell t
-        evalBF t' bcs
+        let (s', t') = printCell s t
+         in evalBF s' t' bcs
       ReadCell -> do
         t' <- readCell t
-        evalBF t' bcs
-      Comment s -> evalBF t bcs
+        evalBF s t' bcs
+      Comment com -> evalBF s t bcs
+
+tests :: [(String, String)]
+tests =
+  [ ("+[-->-[>>+>-----<<]<--<---]>-.>>>+.>>..+++[.>]<<<<.+++.------.<<-.>>>>+.", "Hello, World!"),
+    ("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.", "Hello World!\n"),
+    ("++++[>++++++<-]>[>+++++>+++++++<<-]>>++++<[[>[[>>+<<-]<]>>>-]>-[>+>+<<-]>]+++++[>+++++++<<++>-]>.<<.", "#\n"),
+    ("[]++++++++++[>>+>+>++++++[<<+<+++>>>-]<<<<-]\nA*$\";?@![#>>+<<]>[>>]<<<<[>++<[-]]>.>.", "H\n")
+  ]
+
+runTest :: String -> String -> IO String
+runTest input expected = case parse parseBF "test" (fromString input) of
+  Left pe -> return "Failed parsing"
+  Right bcs -> do
+    (result, _) <- evalBF "" newBFTape bcs
+    return $ if result == expected then "Pass" else "Fail"
+
+runTests :: IO [String]
+runTests = mapM (uncurry runTest) tests
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
+    ["test"] -> do
+      ss <- runTests
+      print ss
     [fn] -> do
       contents <- readFile fn
       case parse parseBF "main" (fromString contents) of
         Left pe -> print pe
-        Right bcs -> do evalBF newBFTape bcs; putStrLn ""
+        Right bcs -> do
+          (s, _) <- evalBF "" newBFTape bcs
+          putStrLn s
     _ -> do
       progName <- getProgName
       putStrLn $ "USAGE: " ++ progName ++ " " ++ "FILENAME"
